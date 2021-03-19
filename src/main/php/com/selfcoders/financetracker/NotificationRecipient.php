a68e0bfd80e19d8da64fc102cc468c8ca2d76cd3
Michael Wieland <git@selfcoders.com>
2021-03-19 22:02:36 +0100

    Use different notification sounds for low/high limit reached in Pushover notification

--- src/main/php/com/selfcoders/financetracker/NotificationRecipient.php
+++ src/main/php/com/selfcoders/financetracker/NotificationRecipient.php
@@ -10,7 +10,8 @@ use Swift_SmtpTransport;
 class NotificationRecipient
 {
     const TYPE_EMAIL = "email";
-    const TYPE_PUSHOVER = "pushover";
+    const TYPE_PUSHOVER_LOW = "pushover_low";
+    const TYPE_PUSHOVER_HIGH = "pushover_high";
 
     public string $type;
     public string $target;
@@ -38,36 +39,49 @@ class NotificationRecipient
 
         switch ($this->type) {
             case self::TYPE_EMAIL:
-                $smtpPort = getenv("SMTP_PORT");
-                if ($smtpPort === false) {
-                    $smtpPort = 25;
-                }
-
-                $transport = new Swift_SmtpTransport(getenv("SMTP_HOST"), $smtpPort, "tls");
-                $transport->setUsername(getenv("SMTP_USERNAME"));
-                $transport->setPassword(getenv("SMTP_PASSWORD"));
-
-                $mailer = new Swift_Mailer($transport);
-
-                $message = new Swift_Message($subject);
-                $message->setFrom(getenv("MAIL_FROM"));
-                $message->setTo($this->target);
-                $message->setBody($body);
-
-                $mailer->send($message);
+                $this->sendMail($subject, $body);
                 break;
-            case self::TYPE_PUSHOVER:
-                $client = new Client;
-                $client->post("https://api.pushover.net/1/messages.json", [
-                    RequestOptions::FORM_PARAMS => [
-                        "token" => getenv("PUSHOVER_TOKEN"),
-                        "user" => $this->target,
-                        "message" => $body,
-                        "title" => $subject,
-                        "sound" => getenv("PUSHOVER_SOUND")
-                    ]
-                ]);
+            case self::TYPE_PUSHOVER_LOW:
+                $this->sendPushover($subject, $body, getenv("PUSHOVER_LOW_SOUND"));
+                break;
+            case self::TYPE_PUSHOVER_HIGH:
+                $this->sendPushover($subject, $body, getenv("PUSHOVER_HIGH_SOUND"));
                 break;
         }
+    }
+
+    private function sendMail(string $subject, string $body)
+    {
+        $smtpPort = getenv("SMTP_PORT");
+        if ($smtpPort === false) {
+            $smtpPort = 25;
+        }
+
+        $transport = new Swift_SmtpTransport(getenv("SMTP_HOST"), $smtpPort, "tls");
+        $transport->setUsername(getenv("SMTP_USERNAME"));
+        $transport->setPassword(getenv("SMTP_PASSWORD"));
+
+        $mailer = new Swift_Mailer($transport);
+
+        $message = new Swift_Message($subject);
+        $message->setFrom(getenv("MAIL_FROM"));
+        $message->setTo($this->target);
+        $message->setBody($body);
+
+        $mailer->send($message);
+    }
+
+    private function sendPushover(string $title, string $message, string $sound)
+    {
+        $client = new Client;
+        $client->post("https://api.pushover.net/1/messages.json", [
+            RequestOptions::FORM_PARAMS => [
+                "token" => getenv("PUSHOVER_TOKEN"),
+                "user" => $this->target,
+                "title" => $title,
+                "message" => $message,
+                "sound" => $sound
+            ]
+        ]);
     }
 }
