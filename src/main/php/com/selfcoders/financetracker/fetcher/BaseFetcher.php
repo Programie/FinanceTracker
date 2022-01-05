6c8782e762fafd1e783f46beeb3d00579aa9eabe
Michael Wieland <git@selfcoders.com>
2022-01-05 17:12:27 +0100

    Use CoinMarketCap API if isin is prefixed with "CMC:"

--- src/main/php/com/selfcoders/financetracker/fetcher/BaseFetcher.php
+++ src/main/php/com/selfcoders/financetracker/fetcher/BaseFetcher.php
@@ -3,13 +3,17 @@ namespace com\selfcoders\financetracker\fetcher;
 
 abstract class BaseFetcher implements Fetcher
 {
+    public const ISIN_PREFIX_CRYPTO = "CRYPTO:";
+    public const ISIN_PREFIX_COINMARKETCAP = "CMC:";
     public const DATASOURCE_ING = "ing";
     public const DATASOURCE_LS = "ls";
 
     public static function getFetcherClass(string $isin, string $dataSource = BaseFetcher::DATASOURCE_ING): ?string
     {
-        if (str_starts_with($isin, "CRYPTO:")) {
+        if (str_starts_with($isin, self::ISIN_PREFIX_CRYPTO)) {
             return CryptoFetcher::class;
+        } elseif (str_starts_with($isin, self::ISIN_PREFIX_COINMARKETCAP)) {
+            return CoinMarketCapFetcher::class;
         } elseif ($dataSource === BaseFetcher::DATASOURCE_ING) {
             return INGFetcher::class;
         } elseif ($dataSource === BaseFetcher::DATASOURCE_LS) {
--- src/main/php/com/selfcoders/financetracker/fetcher/FetcherHelper.php
+++ src/main/php/com/selfcoders/financetracker/fetcher/FetcherHelper.php
@@ -11,13 +11,16 @@ class FetcherHelper
     public static function getData(array $isinWknList, bool $force = false): array
     {
         $cryptoFetcher = new CryptoFetcher;
+        $coinMarketCapFetcher = new CoinMarketCapFetcher;
         $lsFetcher = new LSFetcher;
 
         foreach ($isinWknList as $isinWknEntry) {
             list($isin, $wkn) = $isinWknEntry;
 
-            if (str_starts_with($isin, "CRYPTO:")) {
+            if (str_starts_with($isin, BaseFetcher::ISIN_PREFIX_CRYPTO)) {
                 $cryptoFetcher->add($isin, $wkn);
+            } elseif (str_starts_with($isin, BaseFetcher::ISIN_PREFIX_COINMARKETCAP)) {
+                $coinMarketCapFetcher->add($isin, $wkn);
             } else {
                 $lsFetcher->add($isin, $wkn);
             }
